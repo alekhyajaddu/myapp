aa2c8d4d9096c8c819db9428f27e07dda959e699
alekhyajaddu <alekhyajaddu@gmail.com>
2020-03-26 22:55:57 -0500

    edited index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Alekhya Jaddu' });
+  res.render('index', { title:'Alekhyaa Jaddu'});
 });
 
 module.exports = router;
